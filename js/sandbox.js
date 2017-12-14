1014bfdbca987bf500ebe392be73e86fef820f52
Esteban Vihaio <a.de.ruijter@mboutrecht.nl>
2017-12-14 09:40:44 +0100

    week6 do 14-12-2014 midden les ochtend

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,20 +1,25 @@
 // section#js03 arrays
 // ******************************************************************************
-var fruit = ["appel", "peer", "meloen"];      
+var fruit = ["appel", "peer", "meloen"];
+var p_tagAdd = document.getElementById("arrayAdd");
+
+p_tagAdd.innerHTML = show_new_array();
+
+function show_new_array() {
+  var output = "<ol>";
+  for ( var i = 0; i < fruit.length; i++) {
+    output += "<li>" + fruit[i] + "</li>";
+  }
+  output += "</ol>";
+  return output;
+}
 
 function show_array_ol() {
-  // alert("Hoi");
   var p_tag = document.getElementById("arrayTestOl");
-  var output = "<ol>";
-  for (var i = 0; i < fruit.length; i++) {
-    output = output + "<li>" + fruit[i] + "</li>";
-  }
-  output = output + "</ol>"
-  p_tag.innerHTML = output;
+  p_tag.innerHTML = show_new_array();
 }
 
 function show_array() {
-  // alert("Hoi");
   var p_tag = document.getElementById("arrayTest");
   p_tag.innerHTML = "Dit is de inhoud van mijn fruit-array: " + fruit + "<br>" +
                     "Er zitten nu " + fruit.length + " elementen in mijn array";
@@ -31,6 +36,16 @@ function namePushButton() {
       var p_tag = document.getElementById("outputPara");
       // Verander de tekst in het p-tag met innerHTML
       p_tag.innerHTML = "Mijn naam is: " + inputTekst.value;
+}
+
+function add_array_end() {
+  // alert("All your base are belong to us!");
+  var inputAddEnd = document.getElementById("inputAddEnd");
+
+  // https://www.w3schools.com/js/js_array_methods.asp
+  fruit.push(inputAddEnd.value);
+  p_tagAdd.innerHTML = show_new_array();
+  inputAddEnd.value = "";
 }
 
 // *********************************************************************************************
